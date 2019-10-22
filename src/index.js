9037780a599d310847458c6737f3771c266e802d
JAE MELANEE BUGARIN (SHP-IRIS-ISD-OOCLL/MNL) <BUGARJA@OOCL.COM>
2019-10-22 17:20:57 +0800

    modify all react logic and state to redux

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ import { Provider } from "react-redux";
 // if action in CounterGroup want to dispath action from component to ./reducer
 // we need creat a store by pass our customized reducer
 // and pass this store to Provider, we need to that to connect react and redux so reducer can manage the state and trigger react component render() after state change
-const store = createStore(reducer);
+const store = createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 ReactDOM.render(
